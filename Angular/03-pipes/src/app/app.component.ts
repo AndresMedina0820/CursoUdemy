b255f89eb85ab223b5ca1f9ce4231ba96aa7084b
Andres Medina <Andres.Medina@OMNICON.CC>
2018-07-27 17:24:32 -0500

    Seccion 6 pipes cap 81

---
+++ Angular/03-pipes/src/app/app.component.ts
@@ -0,0 +1,34 @@
+import { Component } from '@angular/core';
+import { resolve } from 'dns';
+import { reject } from '../../node_modules/@types/q';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  
+  nombre = "Andres";
+  arreglo = [1,2,3,4,5,6,7,8,9,10];
+  PI = Math.PI;
+  porc = 0.58778;
+  salario = 1234.5;
+  heroe = {
+    nombre: "Spiderman",
+    clave: "Peter Paker",
+    edad: 20,
+    direccion:{
+      pais: "USA",
+      ciudad: "new york",
+      calle: "Primera",
+      casa: "5",
+    }
+  }
+  valorDePromesa = new Promise( (resolve, reject)=>{
+    setTimeout( ()=>resolve('¡Llego la data!'), 3500 );
+  })
+
+  fecha = new Date();
+
+}
